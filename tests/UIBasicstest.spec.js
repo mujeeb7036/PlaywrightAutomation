0de0bd75401b45538a4d4bbce26f3ee9764403bd
Mujeeb Ur Rahaman <104260110+mujeeb7036@users.noreply.github.com>
2022-12-28 11:27:13 +0530

    First Changes

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,48 @@
+const { test, expect } = require('@playwright/test');
+
+
+test.only('Browser Context Playwright Test', async ({ browser }) => {
+    
+    //chrome - plugins/cookies
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    const userName = page.locator('#username');
+    const signIn = page.locator('#signInBtn');
+    const cardTitles = page.locator('.card-body a');
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    //CSS 
+    await userName.type("rahulshetty");
+    await page.locator('[type$="password"]').type('learning');
+    await signIn.click();
+    //webdriverwait
+    console.log(await page.locator('[style*="block"]').textContent());
+    await expect(page.locator('[style*="block"]')).toContainText('Incorrect');
+    //type - fill - used for entering text
+    //clearing already entered text
+    await userName.fill("");
+    // entering the text 
+    await userName.fill("rahulshettyacademy");
+    //clicking button
+    await signIn.click();
+    // grabbing the text of product
+    console.log(await cardTitles.first().textContent());
+    console.log(await cardTitles.nth(1).textContent());
+    //grabbing the text of all products 
+    const allTitles = (await cardTitles.allTextContents());
+    console.log(allTitles);
+
+
+
+
+});
+
+test('Page Playwright Test', async ({ page }) => {
+
+    await page.goto("https:google.com");
+    //get titile - assertion
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Google");
+
+});
